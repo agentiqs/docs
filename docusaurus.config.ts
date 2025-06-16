8bf930a7721fd1e61a2caad989d378759fe4ebe1
Guido <guido@agentiqs.ai>
2025-06-16 13:59:16 -0300

    revert

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://agentiqs.ai',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
   // Make sure assets use relative paths
   trailingSlash: false,
 
