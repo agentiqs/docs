8c03e511bf87c03b3c6a73ec9c28fa3f22495ec3
Guido <guido@agentiqs.ai>
2025-06-16 14:29:04 -0300

    fix index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,11 @@ import LandingPage from '@site/src/components/landing/LandingPage';
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <p>Welcome to Agentiqs.ai</p>
+    <div>
+      <p>Welcome to Agentiqs.ai</p>
+      The landing page is in a different project. You're not supposed to serve this page.<br/>
+      <a href="/docs">Go to the documentation</a><br/>
+      <a href="/blog">Go to the blog</a><br/>
+    </div>
   );
 }
