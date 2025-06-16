2cb4c59397f24bf5443419213d03f8d1439e80c5
Guido <guido@agentiqs.ai>
2025-06-16 17:40:23 -0300

    social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/agentiqs-social-card.jpg',
     navbar: {
       title: 'Agentiqs',
       logo: {
